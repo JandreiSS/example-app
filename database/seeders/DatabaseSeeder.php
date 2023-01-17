d682f47365c8c66bd90783b99f2aa473c4cede68
JandreiSS <jandreisst@gmail.com>
2023-01-17 16:55:51 -0300

    lesson 28

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,52 +17,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
-        Category::truncate();
-        Post::truncate();
+        $user = User::factory()->create([
+            'name' => 'Joe Doe'
+        ]);
+        Post::factory(5)->create([
+            'user_id' => $user->id
+        ]);
         
-        $user = User::factory()->create();
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
-        ]);
-
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
-        ]);
-
-        $hobbies = Category::create([
-            'name' => 'Hobbies',
-            'slug' => 'hobbies'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $personal->id,
-            'title' => 'My Personal Post',
-            'slug' => 'my-personal-post',
-            'excerpt' => 'Lorem ipsum dolar sit amet.',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi repudiandae vel blanditiis dolorum, perspiciatis ipsum iste provident adipisci quidem voluptate quas id porro eos ipsam laborum officia vero magnam facilis.',
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => 'My Work Post',
-            'slug' => 'my-work-post',
-            'excerpt' => 'Lorem ipsum dolar sit amet.',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi repudiandae vel blanditiis dolorum, perspiciatis ipsum iste provident adipisci quidem voluptate quas id porro eos ipsam laborum officia vero magnam facilis.',
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $hobbies->id,
-            'title' => 'My Hobbies Post',
-            'slug' => 'my-hobbies-post',
-            'excerpt' => 'Lorem ipsum dolar sit amet.',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi repudiandae vel blanditiis dolorum, perspiciatis ipsum iste provident adipisci quidem voluptate quas id porro eos ipsam laborum officia vero magnam facilis.',
-        ]);
     }
 }
