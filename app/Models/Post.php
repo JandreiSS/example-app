d9e1076164bf6a3cdb4684cfccbf3a3c32d0b08f
JandreiSS <jandreisst@gmail.com>
2023-01-20 17:29:35 -0300

    lesson 29

---
+++ app/Models/Post.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'slug', 'excerpt', 'body', 'category_id'];
+
+    public function category() {
+        // hasOne, hasMany, belongsTo, belongsToMany
+        return $this->belongsTo(Category::class);
+    }
+
+    public function author() {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
