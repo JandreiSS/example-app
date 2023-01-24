1a09ea08aeeabfd32ac6fc7ef46e62299f7f6c09
JandreiSS <jandreisst@gmail.com>
2023-01-24 16:32:02 -0300

    lesson 36

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -7,7 +7,7 @@
   
   <!-- Links -->
 
-  <div x-show="show" class="py-2 absolute w-full bg-gray-100 mt-2 rounded-xl z-50" style="display: none">
+  <div x-show="show" class="py-2 absolute w-full bg-gray-100 mt-2 rounded-xl z-50 overflow-auto max-h-52" style="display: none">
       {{ $slot }}
   </div>
 </div>
