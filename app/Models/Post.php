d8db4bbb7161f90fb305abb1dd5141ec8fa9283a
JandreiSS <jandreisst@gmail.com>
2023-01-25 14:00:14 -0300

    lesson 38

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,13 +9,20 @@ class Post extends Model
 {
     use HasFactory;
 
-    // protected $fillable = ['title', 'slug', 'excerpt', 'body', 'category_id', 'user_id'];
     protected $guarded = [];
 
     protected $with = ['category', 'author'];
 
-    public function category() {
-        // hasOne, hasMany, belongsTo, belongsToMany
+    public function scopeFilter($query, array $filters)
+    {
+        $query->when($filters['search'] ?? false, fn ($query, $search) => 
+            $query
+                ->where('title', 'like', '%' . $search . '%')
+                ->orWhere('body', 'like', '%' . $search . '%'));
+    }
+
+    public function category() 
+    {
         return $this->belongsTo(Category::class);
     }
 
