8353ba74af676e068d23dce19d6c499eb76a67f4
JandreiSS <jandreisst@gmail.com>
2023-01-26 17:10:49 -0300

    lesson 50

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -3,9 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class SessionsController extends Controller
 {
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store()
+    {
+        // Validate the request
+        $attributes = request()->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        // Attempt to authenticate and log in the user based on the provided credentials
+        if (auth()->attempt($attributes)) {
+            // Session fixation
+            session()->regenerate();
+
+            // Redirect with a success flash message
+            return redirect('/')->with('success', 'Welcome Back!');
+        }
+
+        // Auth failed
+        throw ValidationException::withMessages([
+            'email' => 'Your provided credentials could not be verified.'
+        ]);
+    }
+    
     public function destroy()
     {
         auth()->logout();
