ae0fe54514c613069b95ed85d54043cc0da855c7
JandreiSS <jandreisst@gmail.com>
2023-01-27 14:58:02 -0300

    lesson 54

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,23 +1,22 @@
+@props(['comment'])
 <article class="flex bg-gray-200 border border-gray-300 p-6 rounded-xl space-x-4">
                         
     <!-- Avatar -->
     <div class="flex-shrink-0">
-        <img src="https://i.pravatar.cc/60" alt="" width="60" height="60" class="rounded-xl">
+        <img src="https://i.pravatar.cc/60?u={{ $comment->id }}" alt="" width="60" height="60" class="rounded-xl">
     </div>
 
     <!-- Comment data -->
     <div>
         <header class="mb-4">
-            <h3 class="font-bold">John Doe</h3>
+            <h3 class="font-bold">{{ $comment->author->username }}</h3>
             <p class="text-xs">
                 Posted 
-                <time>8 months ago</time>
+                <time>{{ $comment->created_at->diffForHumans() }}</time>
             </p>
         </header>
         <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-            Nihil, corrupti illum doloribus animi illo, sunt, eius impedit sint corporis consectetur modi. 
-            Vitae natus possimus, error sapiente ratione accusantium corrupti vero?
+            {{ $comment->body }}
         </p>
     </div>
 
