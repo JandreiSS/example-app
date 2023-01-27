af93156f0018e9b4c591816979d8f812b4e2b538
JandreiSS <jandreisst@gmail.com>
2023-01-27 16:55:08 -0300

    lesson 57

---
+++ resources/views/posts/_add-comment-form.blade.php
@@ -0,0 +1,33 @@
+@auth
+    <x-panel>
+        <form action="/posts/{{ $post->slug }}/comments" method="post">
+            @csrf
+
+            <header class="flex items-center">
+                <img src="https://i.pravatar.cc/40?u={{ auth()->id() }}" alt="" width="40" height="40" class="rounded-full">
+                <h2 class="ml-4">Want to participate?</h2>
+            </header>
+
+            <div class="mt-6">
+                <textarea 
+                    name="body" 
+                    class="w-full text-sm focus:outline-none focus:ring" 
+                    rows="5" 
+                    placeholder="Quick, things of something to say!"
+                    required></textarea>
+
+                @error('body')
+                    <span class="text-sm text-red-500">{{ $message }}</span>
+                @enderror
+            </div>
+
+            <div class="flex justify-end mt-6 pt-6 border-t border-gray-300">
+                <x-submit-button>Post</x-submit-button>
+            </div>
+        </form>
+    </x-panel>
+@else
+    <p class="font-semibold">
+        <a href="/register" class="text-blue-500 hover:text-blue-600">Register</a> or <a href="/login" class="text-blue-500 hover:text-blue-600">log in</a> to leave a comment.
+    </p>
+@endauth
