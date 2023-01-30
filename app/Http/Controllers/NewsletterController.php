0535d5ef0f3b4f737cf6db7bd8df80e2873b7727
JandreiSS <jandreisst@gmail.com>
2023-01-30 16:27:37 -0300

    lesson 61

--- app/Http/Controllers/NewsletterController.php
+++ app/Http/Controllers/NewsletterController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Services\Newsletter;
 use Exception;
-use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
 
 class NewsletterController extends Controller
@@ -15,10 +14,13 @@ class NewsletterController extends Controller
 
         try {
             $newsletter->subscribe(request('email'));
+            redirect('/')->with('success', 'Cadastro na newsletter realizado');
         } catch (Exception $e) {
             throw ValidationException::withMessages([
-                'email' => 'This email cound not be added to our nesletter list.'
+                'email' => 'This email cound not be added to our newsletter list.'
             ]);
         }
+
+        
     }
 }
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -62,14 +62,12 @@
                                 <img src="/images/mailbox-icon.svg" alt="mailbox letter">
                             </label>
 
-                            <div>
-                                <input id="email" name="email" type="text" placeholder="Your email address"
-                                       class="lg:bg-transparent py-2 lg:py-0 pl-4 focus-within:outline-none">
-    
-                                @error('email')
-                                    <span class="text-sx text-red-500">{{ $message }}</span>
-                                @enderror
-                            </div>
+                            <input id="email" name="email" type="text" placeholder="Your email address"
+                                    class="lg:bg-transparent py-2 lg:py-0 pl-4 focus-within:outline-none">
+
+                            @error('email')
+                                <span class="text-sx text-red-500">{{ $message }}</span>
+                            @enderror
                         </div>
 
                         <button type="submit"
