6323e9527e93ac969ede2c65d17ac5bc61202bc3
JandreiSS <jandreisst@gmail.com>
2023-01-30 14:47:11 -0300

    lesson 58

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,26 @@
 <?php
 
-use App\Http\Controllers\CommentController;
+use MailchimpMarketing\ApiClient;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
-use Illuminate\Support\Facades\Route;
+
+Route::get('ping', function () {
+
+
+  $mailchimp = new ApiClient();
+
+  $mailchimp->setConfig([
+    'apiKey' => config('services.mailchimp.key'),
+    'server' => 'us12'
+  ]);
+
+  $response = $mailchimp->lists->getListMembersInfo('a2a1c67f42');
+
+  dd($response);
+});
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 
